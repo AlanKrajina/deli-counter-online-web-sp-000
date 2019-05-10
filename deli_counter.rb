1ee7cdb373e3521f378c9968ac6f772bfe1d7b94
Alan Krajina <alankrajina88@gmail.com>
2019-05-10 13:04:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,8 @@ def line(katz_deli)
     elsif katz_deli.length != 0
     
     katz_deli.each_with_index { |val,index| 
-    value << val
-    puts "The line is currently: #{index+1}. #{value}"
+    
+    puts "The line is currently: #{index+1}. #{val}"
       
       
     }
