52c365b6ebf441a844b48226034ba014407e6d67
Alan Krajina <alankrajina88@gmail.com>
2019-05-10 12:25:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
     elsif katz_deli.length != 0
-      katz_deli.each_with_index { |val,index| puts "The line is currently: #{index} for #{val}" }
-    
-    
-      puts "The line is currently: #{katz_deli}"
+      katz_deli.each_with_index { |val,index| puts "The line is currently: #{index}. #{val}"}
+      end  
+  
     end
 end
