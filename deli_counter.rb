a5bb25d797cf1e7752516432b3134ecf6325dfd2
Alan Krajina <alankrajina88@gmail.com>
2019-05-10 12:32:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,12 @@
 katz_deli = []
 
 def line(katz_deli)
+  value=[]
   if katz_deli.length == 0
     puts "The line is currently empty."
     elsif katz_deli.length != 0
       katz_deli.each_with_index { |val,index| 
-      
+      value.push(val)
       puts "The line is currently: #{index + 1}. #{val} "
         
       }
