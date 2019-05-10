6a7e9df036bd753ddbe46b91a8dfaeba95c666de
Alan Krajina <alankrajina88@gmail.com>
2019-05-10 13:50:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
     elsif katz_deli.length != 0
-    message
+    message="The line is currently:"
+    
     katz_deli.each_with_index { |val,index| 
     value.push(val)
     numb.push(index)
